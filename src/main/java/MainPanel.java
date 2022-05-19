58ec10f1a8b060197bf353ce8a358b7e9e85725f
ShaiAshCollege464 <81906487+ShaiAshCollege464@users.noreply.github.com>
2022-05-19 11:37:27 +0300

    Update MainPanel.java

--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -8,7 +8,7 @@ public class MainPanel extends JPanel {
         File file = new File(Constants.PATH_TO_DATA_FILE); //this is the path to the data file
         this.setLayout(null);
         this.setBounds(x, y + Constants.MARGIN_FROM_TOP, width, height);
-        JLabel survivedLabel = new JLabel("Survived Status: ");
+        JLabel survivedLabel = new JLabel("Passenger Class: ");
         survivedLabel.setBounds(x + Constants.MARGIN_FROM_LEFT, y, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
         this.add(survivedLabel);
         this.survivedComboBox = new JComboBox<>(Constants.PASSENGER_CLASS_OPTIONS);
