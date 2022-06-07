7f93770e2c25a471c80fcf263e4fd6f8fc232827
shoham sofer <92480018+sofer1445@users.noreply.github.com>
2022-06-07 11:13:22 +0300

    עדכון נוי

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,11 +1,13 @@
 public class Constants {
     public static final String PATH_TO_DATA_FILE = "src/main/resources/titanic.csv";
-    public static final int WINDOW_WIDTH = 600;
-    public static final int WINDOW_HEIGHT = 700;
+    public static final int WINDOW_WIDTH = 700;
+    public static final int WINDOW_HEIGHT = 600;
     public static final String[] PASSENGER_CLASS_OPTIONS = { "All", "1st", "2nd", "3rd"};
+    public static final String[] SEX_PASSENGER = { "All", "male", "female"};
+    public static final String[] Embarked = { "All", "C", "Q", "S"};
     public static final int MARGIN_FROM_TOP = 10;
     public static final int MARGIN_FROM_LEFT = 5;
-    public static final int LABEL_WIDTH = 100;
+    public static final int LABEL_WIDTH = 130;
     public static final int LABEL_HEIGHT = 30;
     public static final int COMBO_BOX_WIDTH = 80;
     public static final int COMBO_BOX_HEIGHT = 30;
--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -1,55 +1,158 @@
-import com.gembox.spreadsheet.SpreadsheetInfo;
-
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MainPanel extends JPanel {
+
     private JComboBox<String> survivedComboBox;
+    private JComboBox<String> passengerSexBox ;
+    private JComboBox<String> passengerEmbarkedBox;
+    private JTextField passengerIDMax ;
+    private JTextField passengerIDMin ;
+    private JTextField passengerName ;
+    private JTextField passengerSibSp ;// אחים ובני זוג
+    private JTextField passengerPrach ; // ילדים והורים
+    private JTextField passengerTicket ;
+    private JTextField passengerFareMax ; // עלות הכרטיס בפואנד
+    private JTextField passengerFareMin;
+    private JTextField passengerCabin ; // מספר תא
+    private JButton searchButton ;
+
+    public void allUserFilter (){
+        JLabel passengerClassLabel = new JLabel("Passenger Class: ");
+        passengerClassLabel.setBounds(5, 0, 150 , 30);//שינוי שם למחלקת נוסע
+        this.add(passengerClassLabel);
+        this.survivedComboBox = new JComboBox<>(Constants.PASSENGER_CLASS_OPTIONS);
+        this.survivedComboBox.setBounds(135,0, Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
+        this.add(this.survivedComboBox);
+        JLabel passengerSex = new JLabel("Passenger Sex: ");
+        passengerSex.setBounds(5, -10, 100, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerSex);
+        this.passengerSexBox = new JComboBox<>(Constants.SEX_PASSENGER);
+        this.passengerSexBox.setBounds(135, 40, 80,30);
+        this.add(this.passengerSexBox);
+
+        JLabel passengerEmbarked = new JLabel("Passenger Embarked: ");
+        passengerEmbarked.setBounds(5, 30, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerEmbarked);
+        this.passengerEmbarkedBox = new JComboBox<>(Constants.Embarked);
+        this.passengerEmbarkedBox.setBounds(135, 80, 80,30);
+        this.add(this.passengerEmbarkedBox);
+
+        JLabel passengerIDMin = new JLabel("Passenger ID MIN: ");
+        passengerIDMin.setBounds(5, 70, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerIDMin);
+        this.passengerIDMin = new JTextField("");
+        this.passengerIDMin.setBounds(135,120,80,30);
+        this.add(this.passengerIDMin);
+
+        JLabel passengerIDMax = new JLabel("Passenger ID MAX: ");
+        passengerIDMax.setBounds(5, 110, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerIDMax);
+        this.passengerIDMax = new JTextField("");
+        this.passengerIDMax.setBounds(135,160,80,30);
+        this.add(this.passengerIDMax);
+
+        JLabel passengerName = new JLabel("Passenger Name: ");
+        passengerName.setBounds(5, 150, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerName);
+        this.passengerName = new JTextField("");
+        this.passengerName.setBounds(135,200,80,30);
+        this.add(this.passengerName);
+
+        JLabel passengerSibSp = new JLabel("Passenger Sib&Sp: ");
+        passengerSibSp.setBounds(5, 190, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerSibSp);
+        this.passengerSibSp = new JTextField("");
+        this.passengerSibSp.setBounds(135,240,80,30);
+        this.add(this.passengerSibSp);
+
+        JLabel passengerPrach = new JLabel("Passenger Prach: ");
+        passengerPrach.setBounds(5, 230, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerPrach);
+        this.passengerPrach = new JTextField("");
+        this.passengerPrach.setBounds(135,280,80,30);
+        this.add(this.passengerPrach);
+
+
+        JLabel passengerTicket = new JLabel("Passenger Ticket: ");
+        passengerTicket.setBounds(5, 270, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerTicket);
+        this.passengerTicket = new JTextField("");
+        this.passengerTicket.setBounds(135,320,80,30);
+        this.add(this.passengerTicket);
+
+        JLabel passengerFareMax = new JLabel("Passenger Max Fare: ");
+        passengerFareMax.setBounds(5, 310, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerFareMax);
+        this.passengerFareMax = new JTextField("");
+        this.passengerFareMax.setBounds(135,360,80,30);
+        this.add(this.passengerFareMax);
+
+        JLabel passengerFareMin = new JLabel("Passenger Min Fare: ");
+        passengerFareMin.setBounds(5, 350, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerFareMin);
+        this.passengerFareMin = new JTextField("");
+        this.passengerFareMin.setBounds(135,400,80,30);
+        this.add(this.passengerFareMin);
+
+        JLabel passengerCabin = new JLabel("Passenger Cabin: ");
+        passengerCabin.setBounds(5, 390, 140, 130);//שינוי שם למחלקת נוסע
+        this.add(passengerCabin);
+        this.passengerCabin = new JTextField("");
+        this.passengerCabin.setBounds(135,440,80,30);
+        this.add(this.passengerCabin);
+
+
+        this.searchButton = new JButton("Search Button");
+        this.searchButton.setBounds(80, 480, 180, 50);
+        this.add(this.searchButton);
+
+        this.searchButton.addActionListener(e  -> {
+
+
+
+        });
+
+
+
+
+    }
+
+
 //    private List<Passenger> passengerList = new ArrayList<>();
 
     public MainPanel (int x, int y, int width, int height) {
+
         File file = new File(Constants.PATH_TO_DATA_FILE);
-        List<Passenger> passengerList = new ArrayList<>();
-        try{
+        this.setLayout(null);
+        this.setBounds(x, y + Constants.MARGIN_FROM_TOP, width, height);
+
+
+        try {
             Scanner scanner = new Scanner(file);
+            List<Passenger> passengerList = new ArrayList<>();
             int index = 0;
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String passenger = scanner.nextLine();
-                if(index != 0){
+                if (index != 0) {
                     Passenger passengerObject = new Passenger(passenger);
 //                    passengerList.add(passengerObject);
                 }
                 index++;
             }
-
+            allUserFilter();
         } catch (Exception e) { //כל חריגה זה קופץ (כי לא הצלחתי להתגבר על הגלישה)
             e.printStackTrace();
         }
-        this.setLayout(null);
-        this.setBounds(x, y + Constants.MARGIN_FROM_TOP, width, height);
-        JLabel passengerClassLabel = new JLabel("Passenger Class: ");
-        passengerClassLabel.setBounds(x + Constants.MARGIN_FROM_LEFT, y, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);//שינוי שם למחלקת נוסע
-        this.add(passengerClassLabel);
-        this.survivedComboBox = new JComboBox<>(Constants.PASSENGER_CLASS_OPTIONS);
-        this.survivedComboBox.setBounds(passengerClassLabel.getX() + passengerClassLabel.getWidth() + 1, passengerClassLabel.getY(), Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
-        this.add(this.survivedComboBox);
-        this.survivedComboBox.addActionListener((e) -> {
-//            if(Constants.PASSENGER_CLASS_OPTIONS[1] == e.getActionCommand()){
-//
-//            }
-            //do whatever you want on change
-
-        });
     }
 
 //    public List<String> getPasClass(List<Passenger> passengers){
 //
-//
-//    }
+
+
 
 }
